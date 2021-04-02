fbe6807f21de61b2abc7fb7736627dc0f5487248
dammonmarrod <68920289+dammonmarrod@users.noreply.github.com>
2021-04-02 20:42:23 +0200

    Create cuestionarioJS.js

---
+++ Quiz/Cisco/cuestionarioJS.js
@@ -0,0 +1,58 @@
+
+//Pagina Ejemplo que quiero
+https://ccnadesdecero.es/ccna1-v7-itn-modulos-4-7-respuestas/
+
+
+//Solución a mostrar cuando pulsemos SUBMIT
+4 ¿Cuál es el propósito de la ventana deslizante de TCP?
+•	Solicitar a un origen que reduzca la velocidad de transferencia de datos. (correcto)
+•	Asegurar que los segmentos lleguen en orden al destino.
+•	Finalizar la comunicación cuando se completa la transmisión de datos.
+•	Informar a un origen que debe retransmitir datos desde un punto específico en adelante.
+
+La ventana deslizante de TCP permite que un dispositivo de destino informe a un origen que debe reducir la velocidad de transmisión. 
+Para lograrlo, el dispositivo de destino reduce el valor incluido en el campo de tamaño de ventana del segmento. 
+Para especificar la retransmisión desde un punto específico en adelante, se utilizan los números de acuse de recibo. 
+Para asegurar que los segmentos lleguen en orden, se utilizan los números de secuencia. 
+Por último, para finalizar la sesión de comunicación, se utiliza un bit de control FIN.
+
+
+
+13 Un técnico puede hacer ping a la dirección IP del servidor web de una empresa remota, pero no puede hacer ping correctamente a la dirección URL del mismo servidor web. ¿Qué utilidad de software puede utilizar para diagnosticar el problema?
+•	ipconfig
+•	netstat
+•	tracert
+•	nslookup (correcto)
+
+Traceroute (tracert) es una utilidad que genera una lista de los saltos que se alcanzaron con éxito a lo largo de la ruta desde el origen al destino. 
+Esta lista puede proporcionar información importante de verificación y solución de problemas. 
+La utilidad ipconfig se utiliza para mostrar los valores de configuración de IP en una PC con Windows. 
+La utilidad Netstat se utiliza para identificar qué conexiones TCP activas están abiertas y ejecutándose en un host en red. 
+Nslookup es una utilidad que permite al usuario consultar manualmente los servidores de nombres para resolver un nombre de host determinado. 
+Esta utilidad también se puede utilizar para solucionar problemas de resolución de nombres y verificar el estado actual de los servidores de nombres.
+
+
+14 ¿Cuáles de las siguientes son dos características de IP? Elija dos opciones.
+•	Vuelve a transmitir los paquetes si se producen errores.
+•	Opera independientemente de los medios de red. (correcta)
+•	No requiere una conexión completa exclusiva. (correcta)
+•	Garantiza la distribución de los paquetes.
+•	Rearma los paquetes desordenados en el orden correcto en el lado del receptor.
+
+
+Explicación: El Protocolo de Internet (IP) es un protocolo de mejor esfuerzo sin conexión. 
+Esto significa que IP no requiere una conexión de un extremo a otro ni garantiza la entrega de paquetes. 
+IP también es independiente de los medios, lo que significa que funciona independientemente de los medios de red que transportan los paquetes.
+
+
+
+
+16 Los usuarios informan que el acceso a la red es lento. Después de preguntar a los empleados, el administrador de red descubrió que un empleado descargó un programa de análisis de terceros para la impresora. ¿Qué tipo de malware puede introducirse, que provoque el rendimiento lento de la red?
+•	Gusano (correcto)
+•	Suplantación de identidad
+•	Virus
+•	Correo no deseado
+
+Explicación: Un especialista en ciberseguridad debe estar familiarizado con las características de los diferentes tipos de malware y ataques que amenazan a una organización.
+
+
